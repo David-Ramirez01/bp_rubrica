f2ebbe9f612db91cc563e79ee22a2415ec00d87d
David-Ramirez01 <112446342+David-Ramirez01@users.noreply.github.com>
2022-11-28 09:42:59 -0500

    Update Pagina.jsx

--- src/Actividad BP/Pagina.jsx
+++ src/Actividad BP/Pagina.jsx
@@ -138,7 +138,7 @@ const Pagina = () => {
   return(
     <>
     <div className="container mt-5">
-      <h1 className='text-center'>Pagina Bp Rubrica</h1>
+      <h1 className='text-center'>Pagina Web Rubrica</h1>
       <hr />
       <div className="row">
         <div className="col-8">
